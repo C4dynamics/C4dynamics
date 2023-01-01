0ebb9b0d786e4f8ff4cae5d2a9b46fb51d6759ed
ziv meri <zivmeri@gmail.com>
2023-01-01 13:47:24 +0200

    return the files and update the readme file

---
+++ cnvrt.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+ft2m        = 1             # 0.3048    # 
+lbft2kgm    = 1             # 4.88243 # 47.8803 to include gravity 
+r2d         = 180 / np.pi 
+d2r         = np.pi / 180 
+

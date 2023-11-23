471efe44b42181a33a3244aa5d4e8bb5f06f1df8
ziv meri <zivmeri@gmail.com>
2023-11-23 18:06:30 +0200

    first upload to gh-pages sphinx docs

--- build/html/_static/custom.js
+++
@@ -1,9 +0,0 @@
-// custom.js
-document.addEventListener('DOMContentLoaded', function () {
-    const body = document.body;
-    const modeToggleBtn = document.getElementById('mode-toggle-btn');
-
-    modeToggleBtn.addEventListener('click', function () {
-        body.classList.toggle('dark-mode');
-    });
-});

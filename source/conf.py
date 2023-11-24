8804523ecda08b5d57cd6df98e45c028b5afbb01
ziv meri <zivmeri@gmail.com>
2023-11-24 12:53:22 +0200

    some additional updates to sphinx.

--- source/conf.py
+++ source/conf.py
@@ -1,3 +1,6 @@
+import re
+import os, sys
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -14,11 +17,33 @@ release = '0.0.40'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.coverage',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.graphviz',
+    'sphinx.ext.ifconfig',
+    'matplotlib.sphinxext.plot_directive',
+    'IPython.sphinxext.ipython_console_highlighting',
+    'IPython.sphinxext.ipython_directive',
+    'sphinx.ext.mathjax',
+    'sphinx_design',]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
+sys.path.append(os.path.join('..', '..'))
+# print(os.getcwd())
+import c4dynamics
+
+# The short X.Y version (including .devXXXX, rcX, b1 suffixes if present)
+version = re.sub(r'(\d+\.\d+)\.\d+(.*)', r'\1\2', c4dynamics.__version__)
+version = re.sub(r'(\.dev\d+).*?$', r'\1', version)
+# The full version, including alpha/beta/rc tags.
+release = c4dynamics.__version__
+print("%s %s" % (version, release))
 
 
 # -- Options for HTML output -------------------------------------------------
